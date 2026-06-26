bc4e23ae552e4a3e1c788e4f31568cd3f8834f15
Justin Jaffray <justin.jaffray@gmail.com>
2026-06-25 22:13:19 -0400

    update for home

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -15,34 +15,34 @@ return {
     url = "https://tangled.org/ronshavit.com/mini.diff.jj",
   },
 
-  {
-    "epwalsh/obsidian.nvim",
-    version = "*",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("obsidian").setup({
-        workspaces = {
-          {
-            name = "work",
-            path = "~/Documents/justin",
-          },
-        },
-
-        daily_notes = {
-          folder = "Daily Notes",
-          date_format = "%Y %b %d",
-          template = "Daily Note",
-          pass_over_todos = true,
-        },
-
-        templates = {
-          folder = "Templates",
-        },
-
-        follow_url_func = function(url)
-          vim.fn.jobstart({ "open", url })
-        end,
-      })
-    end,
-  },
+  -- {
+  --   "epwalsh/obsidian.nvim",
+  --   version = "*",
+  --   dependencies = { "nvim-lua/plenary.nvim" },
+  --   config = function()
+  --     require("obsidian").setup({
+  --       workspaces = {
+  --         {
+  --           name = "work",
+  --           path = "~/Documents/justin",
+  --         },
+  --       },
+  --
+  --       daily_notes = {
+  --         folder = "Daily Notes",
+  --         date_format = "%Y %b %d",
+  --         template = "Daily Note",
+  --         pass_over_todos = true,
+  --       },
+  --
+  --       templates = {
+  --         folder = "Templates",
+  --       },
+  --
+  --       follow_url_func = function(url)
+  --         vim.fn.jobstart({ "open", url })
+  --       end,
+  --     })
+  --   end,
+  -- },
 }
